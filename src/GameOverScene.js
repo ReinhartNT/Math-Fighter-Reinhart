f01eb3dd86dc70ea2d800876a11522ce00d9c4f9
Reinhart <cloudofpigs@gmail.com>
2023-10-26 17:15:41 +0700

    init

---
+++ src/GameOverScene.js
@@ -0,0 +1,27 @@
+import Phaser from "phaser";
+
+export default class GameOverScene extends Phaser.Scene {
+    constructor(){
+        super('over-scene')
+    }
+    init(data){
+        this.replayButton = undefined
+        this.score = data.score
+    }
+    preload(){
+        this.load.image('background', 'images/bg_layer1.png')
+        this.load.image('game-over', 'images/gameover.png')
+        this.load.image('replay-button', 'images/replay.png')
+    }
+    create(){
+        this.add.image(240, 320, 'background')
+        this.add.image(240, 240, 'game-over')
+        this.add.text(165, 320, 'Score: ' + this.score, {
+            fontSize: '32px', color: 'black'
+        })
+        this.replayButton = this.add.image(230, 400, 'replay-button').setInteractive().setScale(0.5)
+        this.replayButton.once('pointerup', () => {
+            this.scene.start('math-fighter-scene')
+        }, this)
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,19 +1,25 @@
 import Phaser from 'phaser'
 
 import HelloWorldScene from './HelloWorldScene'
+import MathFighterScene from './MathFighterScene'
+import GameOverScene from './GameOverScene'
 
 const config = {
 	type: Phaser.AUTO,
 	parent: 'app',
-	width: 800,
-	height: 600,
+	width: 480,
+	height: 640,
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: { y: 200 },
 		},
 	},
-	scene: [HelloWorldScene],
+	scene: [MathFighterScene, GameOverScene],
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH
+	}
 }
 
 export default new Phaser.Game(config)
